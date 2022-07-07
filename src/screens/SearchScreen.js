9e9e2715029cb4625fd97ef7d6c3587d1e86400a
Mike R <or4ngedeuce@gmail.com>
2022-07-06 21:01:47 -0500

    Create and style new input search field

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,16 +1,26 @@
-import React from "react";
-import { View, Text, Stylesheet } from "react-native";
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
 import SearchBar from "./components/SearchBar";
 
 const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+
   return (
     <View>
-      <SearchBar />
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() =>
+          console.log("term was submitted via onTermSubmitted!")
+        }
+      />
       <Text>Search Screen</Text>
+      <Text>Here is what user is typing:</Text>
+      <Text>{term}</Text>
     </View>
   );
 };
 
-//const styles = Stylesheet.create({});
+const styles = StyleSheet.create({});
 
 export default SearchScreen;
--- src/screens/components/SearchBar.js
+++ src/screens/components/SearchBar.js
@@ -1,17 +1,45 @@
 import React from "react";
-import { View, Text, Stylesheet } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons";
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
-    <View style={styles.background}>
-      <Text>Search Bar</Text>
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        autoCorrect={false}
+        autoCapitalize="none"
+        placeholder="Search"
+        style={styles.inputStyle}
+        value={term}
+        //onChangeText={(newTerm) => onTermChange(newTerm)}
+        onChangeText={onTermChange}
+        //onEndEditing={() => onTermSubmit()}
+        onEndEditing={onTermSubmit}
+      />
     </View>
   );
 };
 
-const styles = Stylesheet.create({
-  background: {
-    backgroundColor: "#F0EEEE",
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 15,
+    backgroundColor: "#F0EE00",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 20,
+    flexDirection: "row",
+  },
+  inputStyle: {
+    //borderColor: "black",
+    //borderWidth: 1,
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 10,
   },
 });
 
