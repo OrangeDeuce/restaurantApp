c948b684c08f06d7164d346b742275bb0ec0b7de
Mike R <or4ngedeuce@gmail.com>
2022-07-09 09:50:38 -0500

    Apply styling to Name and Title

--- src/screens/components/ResultsDetail.js
+++ src/screens/components/ResultsDetail.js
@@ -4,11 +4,25 @@ import { View, Image, Text, StyleSheet } from "react-native";
 const ResultsDetail = ({ result }) => {
   return (
     <View>
+      <Image style={styles.image} source={{ uri: result.image_url }}></Image>
       <Text>{result.name}</Text>
+      <Text>
+        {result.rating} Stars, {result.review_count} Reviews{" "}
+      </Text>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 250,
+    height: 120,
+    borderRadius: 4,
+  },
+  name: {
+    fontWeight: "bold",
+    // fontSize: 16,
+  },
+});
 
 export default ResultsDetail;
