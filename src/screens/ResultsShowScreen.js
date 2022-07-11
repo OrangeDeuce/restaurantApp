920dea9ad49be421393a105fffc0333347199896
Mike R <or4ngedeuce@gmail.com>
2022-07-11 09:18:37 -0500

    Apply fetching images at new screen

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,16 +1,46 @@
-import React from "react";
-import { View, Image, StyleSheet, Text } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Image, StyleSheet, Text, FlatList } from "react-native";
+import yelp from "../api/yelp";
 
 const ResultsShowScreen = ({ navigation }) => {
   const id = navigation.getParam("id");
-  console.log(id); //Just to test if id shows up successfully
+  //console.log(id); //Just to test if id shows up successfully
+  const [result, setResult] = useState(null);
+
+  console.log(result);
+
+  const getResult = async (id) => {
+    const response = await yelp.get(`/${id}`);
+    setResult(response.data);
+  };
+
+  useEffect(() => {
+    getResult(id);
+  }, []);
+
+  if (!result) {
+    return null;
+  }
+
   return (
     <View>
-      <Text>The Results Show Screen</Text>
+      <Text>{result.name}</Text>
+      <FlatList
+        data={result.photos}
+        keyExtractor={(photo) => photo}
+        renderItem={({ item }) => {
+          return <Image style={styles.image} source={{ uri: item }} />;
+        }}
+      />
     </View>
   );
 };
 
-const style = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    width: 300,
+    height: 200,
+  },
+});
 
 export default ResultsShowScreen;
